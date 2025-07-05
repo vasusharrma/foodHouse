a5ad2981c398ff5ad6899d39385b37c33e7c3952
vasusharrma <sharmavashu099@gmail.com>
2025-07-05 20:38:45 +0530

    initilized with cdn links

---
+++ App.js
@@ -0,0 +1,18 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const structure = React.createElement("div", { className: "parent"   }, [
+  React.createElement(
+    "h1",
+    { id: "child1" , key:"1el" },
+    "i am an h1 tag created by react "
+  ),
+  React.createElement(
+    "h2",
+    { id: "child2" , key:"2el"},
+    "i am an h2 tag created by react"
+  ),
+]);
+
+const grandParent = React.createElement('div' , {className:"grandParent"},   structure , structure);
+
+root.render(grandParent);
