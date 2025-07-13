145e646280480e15ba877ec6a96592a11bff6ad0
vasusharrma <sharmavashu099@gmail.com>
2025-07-13 08:48:12 +0530

    search functionality with backup data

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,11 +5,13 @@ import Restaurants from "./Restaurants.jsx";
 const Body = () => {
   const [cards, setCards] = useState([]);
   const [defaultCards, setDefaultCards] = useState([]);
+  const [isSearchOn, setIsSearchOn] = useState(false);
   const [buttonText, setButtonText] = useState("Top Rated");
   const [currCity, setCurrCity] = useState([
     30.015538499999998,
     77.60388038805745,
   ]);
+  const [searchRes, setSearchRes] = useState("");
 
   const [filtered, setFiltered] = useState(false);
 
@@ -21,15 +23,32 @@ const Body = () => {
     );
     const fetchedDataJson = await fetchedData.json();
 
-    const arr = fetchedDataJson?.data?.cards[1]?.card?.card?.gridElements
-      ?.infoWithStyle
-      ?.restaurants;
+    let arr = [];
+    for (let i = 0; i < fetchedDataJson?.data?.cards.length; i++) {
+      const temparr = fetchedDataJson?.data?.cards[i]?.card?.card?.gridElements
+        ?.infoWithStyle
+        ?.restaurants;
+      if (temparr) {
+        arr = [...arr, ...temparr];
+      }
+    }
+
     if (arr) {
       const newCards = new Set([...cards, ...arr]);
       setCards([...newCards]);
       setDefaultCards([...newCards]);
     }
   };
+
+  function searchResFun() {
+    const filterCards = cards.filter((card) =>
+      card.info.name.includes(searchRes)
+    );
+
+    setIsSearchOn(true);
+    setSearchRes("");
+    setCards(filterCards);
+  }
 
   function topRated() {
     if (filtered) {
@@ -46,28 +65,66 @@ const Body = () => {
   useEffect(() => {
     fetchCards();
   }, []);
-  return (
-    <div className="w-[90%] md:w-[80%] xl:w-[70%] mx-auto">
-      <div className="flex">
+  return (isSearchOn && (cards.length === 0))
+    ? (
+      <div className="flex flex-col items-center justify-around gap-5">
+        <p>404 Not Found</p>
         <button
           type="button"
-          onClick={topRated}
+          onClick={() => {
+            setIsSearchOn(false);
+            const timeoutid = setTimeout(() => {
+              setCards(defaultCards);
+              clearTimeout(timeoutid);
+            }, 100);
+          }}
           className="bg-[#ED3500] text-white px-3 py-2  font-bold cursor-pointer"
         >
-          {buttonText}
+          back to page
         </button>
       </div>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] place-items-center">
-        {cards.length
-          ? (cards.map((card, index) => (
-            <Restaurants
-              key={index}
-              info={card.info}
+    )
+    : (
+      <div className="w-[90%] md:w-[80%] xl:w-[70%] mx-auto">
+        <div className="flex justify-between">
+          <button
+            type="button"
+            onClick={topRated}
+            className="bg-[#ED3500] text-white px-3 py-2  font-bold cursor-pointer"
+          >
+            {buttonText}
+          </button>
+
+          <div className="flex justify-center items-center gap-4">
+            <input
+              type="text"
+              placeholder="enter a restaurant.."
+              className="h-6 border-b outline-none focus:outline:none  p-3 text-lg "
+              onChange={(e) => {
+                setSearchRes(e.target.value);
+              }}
+              value={searchRes}
             />
-          )))
-          : <Shimmer />}
+            <button
+              type="button"
+              onClick={searchResFun}
+              className="bg-[#ED3500] text-white px-3 py-2  font-bold cursor-pointer"
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] place-items-center">
+          {cards.length
+            ? (cards.map((card, index) => (
+              <Restaurants
+                key={index}
+                info={card.info}
+              />
+            )))
+            : <Shimmer />}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 export default Body;
